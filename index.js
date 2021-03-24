48058c1b7be70652e610ae4876cae97d2a0259f0
Artem <artem0793@gmail.com>
2021-03-25 01:40:38 +0200

    c3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Test");
+const http = require("http");
+
+const host = "localhost";
+const port = 8000;
+
+function Route(name, path, methods, callback) {
+	if (!this) {
+		return new Route(name, path, methods, callback);
+	}
+
+	this.name = name;
+	this.path = path;
+	this.methods = methods;
+	this.callback = callback;
+}
+
+
+const server = http.createServer((request, response) => {
+    response.end("test");
+});
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
